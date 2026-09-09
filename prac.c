7fcbbfdfafc9d1efec05082230084b8160024349
rudransh-bhardwaj09 <rudransh.bhardwaj09@gmail.com>
2026-09-09 12:00:00 +0730

    codes

---
+++ prac.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n & 1)
+        printf("Odd");
+    else
+        printf("Even");
+
+    return 0;
+}
